c08daf6211c3e6799980be161c56b972615dca16
Gareth <gareth.kh.lau@gmail.com>
2021-05-08 10:38:57 -0600

    Implement update Image endpoint

--- src/routes/images.ts
+++ src/routes/images.ts
@@ -6,10 +6,10 @@ import sharp from 'sharp';
 import { S3_BUCKET_NAME } from '../config';
 import sizeOf from 'image-size';
 import { parseSizes, parseTags } from '../utils/parser';
-
+import { Image } from '../interfaces/Image';
 import variantsRouter from './variants';
-import logger from '../utils/logger';
 import validObjectId from '../middleware/validObjectId';
+import { FilterQuery } from 'mongoose';
 
 const upload = multer();
 const router = Router();
@@ -17,17 +17,32 @@ const router = Router();
 router.use('/:imageId/variants', variantsRouter);
 
 // Get all images
-router.get('/', async (req, res) => {
+router.get('/', async (req, res, next) => {
+  const rawSearch = req.query.search;
+
+  const query: FilterQuery<Image> = {};
+
+  if (rawSearch && rawSearch !== ' ') {
+    const search = req.query.search?.toString();
+    // parse tags from search
+    const tags = search.split(',').filter((tag) => tag && tag !== ' ');
+
+    // filter images by tags
+    if (tags.length > 0) {
+      query.tags = { $in: tags };
+    }
+  }
+
   try {
-    const images = await ImageModel.find().exec();
-    res.send({ images });
+    const images = await ImageModel.find(query).exec();
+    return res.send({ images });
   } catch (error) {
-    return res.status(500).send();
+    return next(error);
   }
 });
 
 // Get a single image
-router.get('/:imageId', validObjectId('imageId'), async (req, res) => {
+router.get('/:imageId', validObjectId('imageId'), async (req, res, next) => {
   try {
     const { imageId } = req.params;
     const image = await ImageModel.findById(imageId).exec();
@@ -36,13 +51,12 @@ router.get('/:imageId', validObjectId('imageId'), async (req, res) => {
     }
     return res.status(200).send({ image });
   } catch (error) {
-    logger.error;
-    return res.status(500).send();
+    return next(error);
   }
 });
 
 // Upload new images
-router.post('/', upload.single('image'), async (req, res) => {
+router.post('/', upload.single('image'), async (req, res, next) => {
   const file = req.file;
 
   let sizes: number[][];
@@ -64,46 +78,50 @@ router.post('/', upload.single('image'), async (req, res) => {
     return res.status(415).send({ message: error });
   }
 
-  // save source image
-  await putObject(S3_BUCKET_NAME, file.originalname, file.buffer);
+  try {
+    // save source image
+    await putObject(S3_BUCKET_NAME, file.originalname, file.buffer);
 
-  // Generate images for requested sizes and source size
-  const promises = sizes.map(
-    ([width, height]) =>
-      new Promise(async (resolve, reject) => {
-        // resize the image
-        sharp(file.buffer)
-          .jpeg({ quality: 80 })
-          .resize(width, height)
-          .toBuffer()
-          .then((resizedBuffer) => {
-            // create file name
-            const filename = `${name}_${width}x${height}.jpg`;
+    // Generate images for requested sizes and source size
+    const promises = sizes.map(
+      ([width, height]) =>
+        new Promise(async (resolve, reject) => {
+          // resize the image
+          sharp(file.buffer)
+            .jpeg({ quality: 80 })
+            .resize(width, height)
+            .toBuffer()
+            .then((resizedBuffer) => {
+              // create file name
+              const filename = `${name}_${width}x${height}.jpg`;
 
-            // save the image
-            putObject(S3_BUCKET_NAME, filename, resizedBuffer)
-              .then(() => {
-                resolve(filename);
-              })
-              .catch(reject);
-          });
-      })
-  );
+              // save the image
+              putObject(S3_BUCKET_NAME, filename, resizedBuffer)
+                .then(() => {
+                  resolve(filename);
+                })
+                .catch(reject);
+            });
+        })
+    );
 
-  const variants = await Promise.all(promises);
+    const variants = await Promise.all(promises);
 
-  // save to mongodb
-  const image = await new ImageModel({
-    src: file.originalname,
-    variants,
-    tags
-  }).save();
+    // save to mongodb
+    const image = await new ImageModel({
+      src: file.originalname,
+      variants,
+      tags
+    }).save();
 
-  return res.status(200).send({ message: '', image });
+    return res.status(200).send({ message: '', image });
+  } catch (error) {
+    return next(error);
+  }
 });
 
 // Delete an image
-router.delete('/:imageId', async (req, res) => {
+router.delete('/:imageId', async (req, res, next) => {
   const { imageId } = req.params;
   try {
     const image = await ImageModel.findById(imageId).exec();
@@ -118,20 +136,37 @@ router.delete('/:imageId', async (req, res) => {
 
     return res.status(200).send({ message: 'Image successfully deleted.' });
   } catch (error) {
-    return res.status(500).send();
+    return next(error);
   }
 });
 
 // Update an image
-router.patch('/:imageId', async (req, res) => {
+router.patch('/:imageId', validObjectId('imageId'), async (req, res, next) => {
   const { imageId } = req.params;
+  const rawTags: string = req.body.tags;
+
+  // object to execute update against
+  const updateQuery: Partial<Image> = {};
+
+  if (rawTags && rawTags !== ' ') {
+    const tags = rawTags.split(',').filter((tag) => tag && tag !== ' ');
+    if (tags.length > 0) {
+      // only override tags if provided with values
+      updateQuery.tags = tags;
+    }
+  }
 
   try {
-    const image = await ImageModel.findById(imageId).exec();
-    // TODO - Update image properties
-    return res.status(200).send({ image });
+    const updatedImage = await ImageModel.findByIdAndUpdate(
+      imageId,
+      updateQuery,
+      {
+        new: true
+      }
+    ).exec();
+    return res.status(200).send({ image: updatedImage });
   } catch (error) {
-    return res.status(500).send();
+    return next(error);
   }
 });
 
